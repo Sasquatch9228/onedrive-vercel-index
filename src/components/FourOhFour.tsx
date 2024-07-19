3bd4e135e697e8e9bd2826f2e489b164a519344b
Matthew6229 <164438014+Sasquatch9228@users.noreply.github.com>
2024-07-19 15:50:31 -0400

    Update FourOhFour.tsx

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -5,13 +5,13 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
   return (
     <div className="my-12">
       <div className="mx-auto w-1/3">
-        <Image src="/images/fabulous-rip-2.png" alt="404" width={912} height={912} priority />
+        <Image src="/images/bigfoot-angry.png" alt="404" width={912} height={912} priority />
       </div>
       <div className="mx-auto mt-6 max-w-xl text-gray-500">
         <div className="mb-8 text-xl font-bold">
           <Trans>
             {/* eslint-disable-next-line react/no-unescaped-entities */}
-            Oops, that's a <span className="underline decoration-red-500 decoration-wavy">four-oh-four</span>.
+            Oops, that's a <span className="underline decoration-red-500 decoration-wavy">four-oh-four</span>. How did you even get here?
           </Trans>
         </div>
         <div className="mb-4 overflow-hidden break-all rounded border border-gray-400/20 bg-gray-50 p-2 font-mono text-xs dark:bg-gray-800">
@@ -23,14 +23,14 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
             <kbd className="rounded border border-gray-400/20 bg-gray-100 px-1 font-mono text-xs dark:bg-gray-800">
               F12
             </kbd>{' '}
-            and open devtools for more details, or seek help at{' '}
+            and open the developer tools for more details, or seek help by emailing me at{' '}
             <a
               className="text-blue-600 hover:text-blue-700 hover:underline"
-              href="https://github.com/spencerwooo/onedrive-vercel-index/discussions"
+              href="mailto:vasquezlister288@gmail.com"
               target="_blank"
               rel="noopener noreferrer"
             >
-              onedrive-vercel-index discussions
+              vasquezlister288@gmail.com
             </a>
             .
           </Trans>
